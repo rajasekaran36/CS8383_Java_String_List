c920929a335da0bb5b0036b79ca4f1e05935c692
Rajasekaran <proffraja@gmail.com>
2018-06-01 15:28:46 +0530

    UI Modified to Accept Choices

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 import com.raja.oopslab.stringlist.StringList;
 
 public class Main {
-	public static void main(String[] args){
-		StringList sl = new StringList();
-		sl.addString("Hello");
-		sl.addString("Hai");
-		sl.addString("Joo");
-		sl.addString("Foo");
-		//sl.displayList();
-		sl.insertStringAt(2, "XXX");
-		//sl.displayList();
-		sl.searchString("99");
-		sl.displayStringsBeginWith("H");
+	public static void main(String[] args) {
+		StringList stringList = new StringList();
+		Scanner userInput = new Scanner(System.in);
+		String aString = null;
+		int choice = 0;
+		int position = 0;
+		while (choice != 6) {
+			System.out.println("\nString List Operations");
+			System.out.println("************************");
+			System.out.println(
+					"1. Append a String\n"
+					+ "2. Insert a String at Position\n"
+					+ "3. Search a String\n"
+					+ "4. List all Strings\n"
+					+ "5. Display String's Begins with a Letter\n"
+					+ "6.Exit");
+
+			choice = userInput.nextInt();
+
+			switch (choice) {
+			case 1:
+				System.out.println("Enter a String: ");
+				stringList.addString(userInput.next());
+				break;
+			case 2:
+				System.out.println("Enter a String: ");
+				aString = userInput.next();
+				System.out.println("Enter a Position to Insert: ");
+				position = userInput.nextInt();
+				stringList.insertStringAt(position, aString);
+				break;
+			case 3:
+				System.out.println("Enter a String to Be Searched: ");
+				aString = userInput.next();
+				stringList.searchString(aString);
+				break;
+			case 4:
+				System.out.println("\nList Contains");
+				System.out.println("-------------");
+				stringList.displayList();
+				break;
+			case 5:
+				System.out.println("Enter a letter");
+				stringList.displayStringsBeginWith(userInput.next());
+				break;
+			case 6:
+				System.out.println("\n\nThank You !!!");
+				userInput.close();
+				System.exit(0);
+				break;
+			default:
+				System.out.println("Please enter valid input");
+				break;
+			}
+		}
 	}
 }
--- src/com/raja/oopslab/stringlist/StringList.java
+++ src/com/raja/oopslab/stringlist/StringList.java
@@ -3,57 +3,69 @@ package com.raja.oopslab.stringlist;
 import java.util.ArrayList;
 
 public class StringList {
-ArrayList<String> listOfStrings;
-public StringList() {
-	listOfStrings = new ArrayList<String>();
-}
+	ArrayList<String> listOfStrings;
 
-public boolean addString(String aString){
-	boolean result = listOfStrings.add(aString);
-	return result;
-}
-public void insertStringAt(int position, String aString){
-	int list_size = listOfStrings.size();
-	int cur_pos = 0;
-	ArrayList<String> templist = new ArrayList<String>();
-	if(position>list_size)
-		System.out.println("Sorry Position is not in the list limit");
-	else {
-		for(String elememt:listOfStrings){
-			
-			if(cur_pos == position)
-				templist.add(aString);
-			
-			templist.add(elememt);
-			cur_pos++;
+	public StringList() {
+		listOfStrings = new ArrayList<String>();
+	}
+
+	public boolean addString(String aString) {
+		boolean result = listOfStrings.add(aString);
+		return result;
+	}
+
+	public void insertStringAt(int position, String aString) {
+		int list_size = listOfStrings.size();
+		int cur_pos = 0;
+		ArrayList<String> templist = new ArrayList<String>();
+		if (position > list_size)
+			System.out.println("Sorry Position is not in the list limit");
+		else {
+			for (String elememt : listOfStrings) {
+
+				if (cur_pos == position)
+					templist.add(aString);
+
+				templist.add(elememt);
+				cur_pos++;
+			}
+		}
+		listOfStrings = templist;
+	}
+
+	public void displayList() {
+		int i = 0;
+		for (String element : listOfStrings){
+			System.out.println(i+"."+element);
+			i++;
 		}
 	}
-	listOfStrings = templist;
-}
-public void displayList(){
-	for(String element:listOfStrings)
-		System.out.println(element);
-}
-public void searchString(String aString){
-	int cur_pos = 0;
-	int foundAt = -1;
-	for(String element:listOfStrings){
-		if(element.equalsIgnoreCase(aString))
-			foundAt = cur_pos;
+
+	public void searchString(String aString) {
+		
+		int cur_pos = 0;
+		int foundAt = -1;
+		
+		
+		for (String element : listOfStrings) {
+			if (element.equalsIgnoreCase(aString)) {
+				foundAt = cur_pos;
+				break;
+			} else
+				cur_pos++;
+		}
+		if (foundAt >= 0)
+			System.out.println("Your string " + aString + " found at position " + cur_pos);
 		else
-			cur_pos++;
+			System.out.println("String not found in the list");
 	}
-	if(foundAt>=0)
-		System.out.println("Your string"+aString+" found at position "+cur_pos);
-	else
-		System.out.println("String not found in the list");
-}
-public void displayStringsBeginWith(String aLetter){
-	
-	for (String element:listOfStrings){
-		String firstLetter  = String.valueOf(element.charAt(0));
-		if(aLetter.equals(firstLetter))
-			System.out.println(element);
+
+	public void displayStringsBeginWith(String aLetter) {
+		System.out.println("The String's Begins with Letter ["+aLetter+"]\n");
+		for (String element : listOfStrings) {
+			String firstLetter = String.valueOf(element.charAt(0));
+			if (aLetter.equals(firstLetter))
+				System.out.println(element);
+		}
 	}
-}
 }
